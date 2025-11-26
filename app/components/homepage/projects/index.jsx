ac1975555508d6dd88390e713de8c677c59d13ca
wissemhammoudi <hammoudiwissem81@gmail.com>
2025-11-26 13:48:47 +0100

    coorecting the project

--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -83,16 +83,25 @@ const Projects = () => {
   const activeFiltersCount = (selectedSkill !== 'all' ? 1 : 0) + (selectedTopic !== 'all' ? 1 : 0);
 
   return (
-    <div id='projects' className="relative z-50 my-12 lg:my-24">
-      <div className="sticky top-10">
-        <div className="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl opacity-30"></div>
-        <div className="flex items-center justify-start relative">
-          <span className="bg-[#1a1443] absolute left-0 w-fit text-white px-5 py-3 text-xl rounded-md">
-            PROJECTS
-          </span>
-          <span className="w-full h-[2px] bg-[#1a1443]"></span>
+    <div id='projects' className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
+      <div className="w-[100px] h-[100px] bg-violet-100 rounded-full absolute top-6 left-[42%] translate-x-1/2 filter blur-3xl  opacity-20"></div>
+
+      <div className="flex justify-center -translate-y-[1px]">
+        <div className="w-3/4">
+          <div className="h-[1px] bg-gradient-to-r from-transparent via-violet-500 to-transparent  w-full" />
         </div>
       </div>
+
+      <div className="flex justify-center my-5 lg:py-8">
+        <div className="flex  items-center">
+          <span className="w-24 h-[2px] bg-[#1a1443]"></span>
+          <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-xl rounded-md">
+            Projects
+          </span> 
+          <span className="w-24 h-[2px] bg-[#1a1443]"></span>
+        </div>
+      </div>
+
 
       <div className="pt-12 max-w-4xl mx-auto px-4">
         <div className="flex flex-wrap items-center gap-4 w-full bg-[#1a1443] p-4 rounded-lg">
